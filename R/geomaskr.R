a6e3ea3165f3f9fd03f7d9c68dbdbf2ec61a4c60
kpkeller <kayleigh.keller@colostate.edu>
2024-03-08 09:02:36 -0700

    add package help file and basic citation

---
+++ R/geomaskr.R
@@ -0,0 +1,6 @@
+#' @name geomaskr-package
+#' @title Geomask Locations in R
+#' @docType package
+#'
+#' @description Provides functions to conduct geomasking of coordinate locations. This includes using a "k-anonymity" approach to selecting a radius for location perturbation.
+NULL
